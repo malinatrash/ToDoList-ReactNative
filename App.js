adbe98f40d9858a1477b5bcaa762f2d9e1b4c8e4
malinatrash <malinatrashh@gmail.com>
2023-01-07 01:22:21 +0800

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
 
 export default function App() {
-  const [result, setresult] = useState("sfs");
   return (
     <View style={styles.container}>
-      <Text>{result}</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}></View>
+
+      </View>
     </View>
   );
 }
@@ -16,7 +17,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
+  tasksWrapper: {
+
+  },
+  sectionTitle: {
+
+  },
+  items: {
+
+  }
 });
